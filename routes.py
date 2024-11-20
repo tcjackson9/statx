af491c504f1a1fa797897cb698ab15c35e505030
Alex Rendler <126621758+alexrendlerCS@users.noreply.github.com>
2024-11-19 20:14:19 -0700

    Merge branch 'main' of https://github.com/alexrendlerCS/statx

--- routes.py
+++ routes.py
@@ -17,7 +17,7 @@ def register_routes(app):
             conn = connect_db()
             cursor = conn.cursor(cursor_factory=RealDictCursor)
             
-            query = "SELECT * FROM player_stats WHERE player_name = %s ORDER BY week"
+            query = "SELECT * FROM player_stats WHERE player_name = %s"
             cursor.execute(query, (player_name,))
             stats = cursor.fetchall()
             cursor.close()
