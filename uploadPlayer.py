0c5d2729c2b3064a7344c1421852eea0db7e933d
Tanner Jackson <tcjackson9@gmail.com>
2024-11-21 18:03:50 -0700

    playerStats

--- uploadPlayer.py
+++ uploadPlayer.py
@@ -65,7 +65,7 @@ def insert_player_stats(player_stats, team_mapping, valid_positions):
     INSERT INTO player_stats (
         player_name, position_id, team_id, week, passing_attempts, completions, 
         passing_yards, passing_tds, interceptions, rushing_attempts, rushing_yards, 
-        rushing_tds, receptions, receiving_yards, receiving_tds
+        rushing_tds, receptions, receiving_yards, receiving_tds, Played
     ) VALUES %s
     ON CONFLICT (player_name, team_id, week) DO UPDATE SET
         position_id = EXCLUDED.position_id,
@@ -79,7 +79,8 @@ def insert_player_stats(player_stats, team_mapping, valid_positions):
         rushing_tds = EXCLUDED.rushing_tds,
         receptions = EXCLUDED.receptions,
         receiving_yards = EXCLUDED.receiving_yards,
-        receiving_tds = EXCLUDED.receiving_tds;
+        receiving_tds = EXCLUDED.receiving_tds,
+        Played = EXCLUDED.Played;
     """
 
 
@@ -93,7 +94,8 @@ def insert_player_stats(player_stats, team_mapping, valid_positions):
                 player.get('PassingAttempts'), player.get('Completions'), player.get('PassingYards'),
                 player.get('PassingTouchdowns'), player.get('Interceptions'),
                 player.get('RushingAttempts'), player.get('RushingYards'), player.get('RushingTouchdowns'),
-                player.get('Receptions'), player.get('ReceivingYards'), player.get('ReceivingTouchdowns')
+                player.get('Receptions'), player.get('ReceivingYards'), player.get('ReceivingTouchdowns'),
+                player.get('Played')
             ))
 
     if values:
